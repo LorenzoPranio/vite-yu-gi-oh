02932f5e4b3cf8cec881bc9f85a323af0d6ae985
LorenzoPranio <lorenzopranio02@gmail.com>
2024-08-01 15:59:46 +0200

    [UPD]Added archetypes array and url

--- src/store.js
+++ src/store.js
@@ -2,5 +2,7 @@ import { reactive } from "vue";
 
 export const cards = reactive({
     apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
-    cardsarray: []
+    apiArchetypes: "https://db.ygoprodeck.com/api/v7/archetypes.php",
+    cardsarray: [],
+    archetypesarray: []
 });
